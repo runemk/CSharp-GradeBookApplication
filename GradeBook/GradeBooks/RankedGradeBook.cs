a5e0eea4add529934c35b70a33fa63d344c16ca5
RuneMK <runekjerri@gmail.com>
2018-03-21 15:02:34 +0100

    d

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,7 +19,14 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            base.CalculateStudentStatistics(name);
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
         }
 
         public override bool Equals(object obj)
