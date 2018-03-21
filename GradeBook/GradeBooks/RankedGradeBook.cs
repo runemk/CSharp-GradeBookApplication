3aa8453cce9fbc3487cc288e49d7c45fe89b6f31
RuneMK <runekjerri@gmail.com>
2018-03-21 15:07:56 +0100

    added override for second method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,7 +14,14 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            base.CalculateStatistics();
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
         }
 
         public override void CalculateStudentStatistics(string name)
