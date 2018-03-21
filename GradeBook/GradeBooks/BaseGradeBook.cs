13b3d97ee84792c56ab8e2e19eafb57ac7dcb368
RuneMK <runekjerri@gmail.com>
2018-03-21 17:06:27 +0100

    weighted grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,18 +109,31 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int grade = 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    grade = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    grade = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    grade = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    grade = 1;
+                    break;
+             
             }
-            return 0;
+            if (this.IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                grade++;
+            }
+
+
+            return grade;
         }
 
         public virtual void CalculateStatistics()
